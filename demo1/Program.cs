634c798804fbeaced649c333a1cd52e9bccd83fc
Maitri-Singh <114772064+Maitri-Singh@users.noreply.github.com>
2025-01-29 02:08:29 -0800

    Add files via upload
    add it please

--- demo1/Program.cs
+++ demo1/Program.cs
@@ -1,25 +1,57 @@
-﻿namespace WiproDay3
+﻿using System.ComponentModel;
+
+namespace ClassMethodStaticDemo
 {
-    class Program
+    internal class Program
     {
+        static void Display(ref int n)//Method Definition
+        {
+            n *= 20;//400
+            Console.WriteLine("Value inside the Method" + n);
+        }
+        static void OutSample(out int n)//Method Definition
+        {
+            // n *= 20;//400
+            int a = 20;
+            n = 20 * a;
+            Console.WriteLine("Value inside the Method" + n);
+        }
+        static int Division(int a, int b, out int r)
+        {
+            r = a % b;//remainder will be returned as out parameter
+            return a / b;//quotient is returning
+        }
+        void Show() //Instance Mthd
+        {
+            Console.WriteLine("Instance Method");
+        }
+        //Method with reurn type
+        int add(int a, int b)
+        {
+            return a + b;
+        }
         private static void Main(string[] args)
         {
-            Console.WriteLine("Enter the ProductId");
-            int id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the ProductName");
-            string nm = Console.ReadLine();
+            
 
-            Product product = new Product(id, nm); // object creation - calling parameterized constructor
-            product.PublicMthd();
+            Employee[] emp = new Employee[5];
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine("Enter EmployeeId:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter EmployeeName:");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter Dept");
+                string dpt = Console.ReadLine();
+                Console.WriteLine("Enter Salary");
+                decimal sal = Convert.ToDecimal(Console.ReadLine());
+                emp[i] = new Employee(id, name, dpt, sal);
 
-
-            Customer cust = new Customer();
-            Console.WriteLine("Enter the CustomerId");
-            cust.custId = Convert.ToInt32(Console.ReadLine()); //assigning the user input to the class member directly since it is public
-            Console.WriteLine("Enter the CustomerName");
-            cust.custName = Console.ReadLine();
-
-            cust.Display();
+            }
+            for (int i = 0; i < emp.Length; i++)
+            {
+                Console.WriteLine(emp[i].EmpId + " " + emp[i].EmpName);
+            }
         }
     }
 }
