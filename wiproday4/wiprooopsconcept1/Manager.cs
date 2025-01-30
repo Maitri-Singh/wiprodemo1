39959c9f45ccd3fc89a79b471cc58bea6b891645
Maitri-Singh <114772064+Maitri-Singh@users.noreply.github.com>
2025-01-30 02:02:23 -0800

    Add files via upload
    add it

---
+++ wiproday4/wiprooopsconcept1/Manager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wiprooopsconcept1
+{
+    internal class Manager : Deparment
+    {
+        public string ManagerName { get; set; }
+        public Manager(int eid, string ename, string dname, decimal sal, string ManagerName) : base(eid, ename, dname, sal)
+        {
+            this.ManagerName = ManagerName;
+        }
+
+        public virtual void Display()
+        {
+            base.Display();
+
+            Console.WriteLine("Mnager name" + ManagerName);
+        }
+    }
+}
---
+++ wiproday4/wiprooopsconcept1/Program.cs
@@ -0,0 +1,18 @@
+﻿using wiprooopsconcept1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int id = Convert.ToInt32(Console.ReadLine());
+        string name = Console.ReadLine();
+        string dname = Console.ReadLine();
+        decimal sal = Convert.ToDecimal(Console.ReadLine());
+        string mname = Console.ReadLine();
+        Manager mgrcon = new Manager(id, name, dname, sal, mname);
+        mgrcon.Display();
+
+
+
+    }
+}
