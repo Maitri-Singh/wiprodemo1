291a6bb4c100cf231e166ceee3a4863af7cd8132
Maitri-Singh <114772064+Maitri-Singh@users.noreply.github.com>
2025-02-03 02:44:55 -0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,57 +1,70 @@
-﻿using System.ComponentModel;
-
-namespace ClassMethodStaticDemo
+﻿internal class Program
 {
-    internal class Program
+    private static void Main(string[] args)
     {
-        static void Display(ref int n)//Method Definition
+        try
         {
-            n *= 20;//400
-            Console.WriteLine("Value inside the Method" + n);
-        }
-        static void OutSample(out int n)//Method Definition
-        {
-            // n *= 20;//400
-            int a = 20;
-            n = 20 * a;
-            Console.WriteLine("Value inside the Method" + n);
-        }
-        static int Division(int a, int b, out int r)
-        {
-            r = a % b;//remainder will be returned as out parameter
-            return a / b;//quotient is returning
-        }
-        void Show() //Instance Mthd
-        {
-            Console.WriteLine("Instance Method");
-        }
-        //Method with reurn type
-        int add(int a, int b)
-        {
-            return a + b;
-        }
-        private static void Main(string[] args)
-        {
-            
-
-            Employee[] emp = new Employee[5];
-            for (int i = 0; i < emp.Length; i++)
+            Console.WriteLine("Outer try block starts.");
+            int res = 10 / int.Parse("0");
+            try
             {
-                Console.WriteLine("Enter EmployeeId:");
-                int id = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter EmployeeName:");
-                string name = Console.ReadLine();
-                Console.WriteLine("Enter Dept");
-                string dpt = Console.ReadLine();
-                Console.WriteLine("Enter Salary");
-                decimal sal = Convert.ToDecimal(Console.ReadLine());
-                emp[i] = new Employee(id, name, dpt, sal);
+                Console.WriteLine("Inner try block starts.");
+                int[] nums = { 10, 11, 12 };
+                Console.WriteLine(nums[10]);
 
             }
-            for (int i = 0; i < emp.Length; i++)
+            catch (IndexOutOfRangeException ex)
             {
-                Console.WriteLine(emp[i].EmpId + " " + emp[i].EmpName);
+                Console.WriteLine("Inner Catch:" + ex.Message);
             }
+
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Outer Catch:" + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("EndProgram");
+        }
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Outer try block starts.");
+            int res = 10 / int.Parse("0");
+            try
+            {
+                Console.WriteLine("Inner try block starts.");
+                int[] nums = { 10, 11, 12 };
+                Console.WriteLine(nums[10]);
+
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Inner Catch:" + ex.Message);
+            }
+
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Outer Catch:" + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("EndProgram");
         }
     }
 }
